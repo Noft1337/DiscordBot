04bb84e936924381e3d66dd8c0690614ad457ef2
Noft1337 <maudri324@gmail.com>
2022-08-14 22:23:00 +0300

    First implementation of queue Added oklesgo command

--- music.py
+++ music.py
@@ -2,6 +2,14 @@ import discord
 from discord.ext import commands
 import youtube_dl
 
+FFMPEG_OPTIONS = {
+            'before_options': '-reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 5',
+            'options': '-vn'
+        }
+YDL_OPTIONS = {'format': 'bestaudio'}
+
+OK = "https://www.youtube.com/watch?v=GD9QURJd6qA&ab_channel=SvenBalthazard"
+
 OPS = {
     "1": u"https://www.youtube.com/watch?v=HRaoYuRKBaA",
     "2": u"https://www.youtube.com/watch?v=54dp8ucsGG8",
@@ -41,17 +49,22 @@ class Music(commands.Cog):
 
     @commands.command(name="join", pass_ctx=True)
     async def join(self, ctx):
+        voice_channel = None
         if ctx.author.voice is None:
             await ctx.send("You're not in a voice channel.")
-        voice_channel = ctx.author.voice.channel
+            return
+        else:
+            voice_channel = ctx.author.voice.channel
         if ctx.voice_client is None:
             await voice_channel.connect()
         else:
             await ctx.voice_client.move_to(voice_channel)
 
     @staticmethod
-    def is_connected(ctx):
-        return ctx.voice_client is not None
+    def is_connected(ctx: discord.ext.commands.context.Context):
+        connected = ctx.voice_client and ctx.voice_client.is_connected()
+        print(f"Connected to voice channel: {ctx.voice_client and ctx.voice_client.is_connected()}")
+        return True if connected else False
 
     @commands.command(name="disconnect", pass_ctx=True)
     async def disconnect(self, ctx):
@@ -61,33 +74,56 @@ class Music(commands.Cog):
     async def op(self, ctx, num):
         try:
             int(num)
-            await ctx.send("Playing %s." % ("op %s" % num))
-            await self.play(ctx, OPS[num])
+            playing = await self.play(ctx, OPS[num])
+            if playing:
+                await ctx.send("Playing %s." % ("op %s" % num))
         except ValueError:
             if num != "all":
                 await ctx.send("Bad argument %s!" % num)
             else:
+                # todo: play all openings shuffled
                 pass
 
-    @commands.command(name="play", pass_ctx=True)
-    async def play(self, ctx, url=""):
+    @staticmethod
+    def is_playing(ctx: discord.ext.commands.context.Context):
+        print("Bot is playing: %s" % ctx.voice_client.is_playing())
+        return ctx.voice_client.is_playing()
+
+    def add_to_queue(self, url):
+        self.queue.append(url)
+
+    async def handle_connected(self, ctx: discord.ext.commands.context.Context, url):
         if not self.is_connected(ctx):
             await ctx.send("Joining channel.")
             await self.join(ctx)
         else:
-            ctx.voice_client.stop()
-        ffmpeg_options = {
-            'before_options': '-reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 5',
-            'options': '-vn'
-        }
-        ydl_options = {'format': 'bestaudio'}
-        vc = ctx.voice_client
+            # todo: check id is playing and if so add to queue
+            if self.is_playing(ctx):
+                self.add_to_queue(url)
 
-        with youtube_dl.YoutubeDL(ydl_options) as ydl:
-            info = ydl.extract_info(url, download=False)
-            url2 = info['formats'][0]['url']
-            source = await discord.FFmpegOpusAudio.from_probe(url2, **ffmpeg_options)
-            vc.play(source)
+    @staticmethod
+    async def handle_queue(ctx: discord.ext.commands.context.Context):
+        await ctx.send("Added song to the queue.")
+
+    @commands.command(name="play", pass_ctx=True)
+    async def play(self, ctx: discord.ext.commands.context.Context, url=""):
+        await self.handle_connected(ctx, url)
+        if len(self.queue) > 0:
+            await self.handle_queue(ctx)
+            return False
+        else:
+            vc = ctx.voice_client
+            with youtube_dl.YoutubeDL(YDL_OPTIONS) as ydl:
+                info = ydl.extract_info(url, download=False)
+                url2 = info['formats'][0]['url']
+                source = await discord.FFmpegOpusAudio.from_probe(url2, **FFMPEG_OPTIONS)
+                vc.play(source)
+            return True
+
+    @commands.command(name="oklesgo", pass_ctx=True)
+    async def oklesgo(self, ctx: discord.ext.commands.context.Context):
+        await self.play(ctx, OK)
+        await ctx.send("Ok, Let's goo")
 
     @commands.command(name="pause", pass_ctx=True)
     async def pause(self, ctx):
