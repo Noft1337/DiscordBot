8ec94bf0340942b20de0d296b26e1a259e7854a7
Noft1337 <maudri324@gmail.com>
2022-08-15 06:45:37 +0300

    Designed the code a little prettier
    ?stop now clears the queue
    removed a code duplicate I found

--- music.py
+++ music.py
@@ -14,7 +14,7 @@ class Music(commands.Cog):
             v queue function
             - searching music via youtube (make it show the top 5 results),
             - skipping function,
-            - stop clears queue
+            v stop clears queue
     """
 
     def __init__(self, client: commands.Bot):
@@ -25,71 +25,10 @@ class Music(commands.Cog):
     def check_q(self):
         return len(self.queue) > 0
 
-    @commands.command(name="join", pass_ctx=True)
-    async def join(self, ctx):
-        voice_channel = None
-        if ctx.author.voice is None:
-            await ctx.send("You're not in a voice channel.")
-            return
-        else:
-            voice_channel = ctx.author.voice.channel
-        if ctx.voice_client is None:
-            await voice_channel.connect()
-        else:
-            await ctx.voice_client.move_to(voice_channel)
-
     @staticmethod
     def is_connected(ctx: discord.ext.commands.context.Context):
         connected = ctx.voice_client and ctx.voice_client.is_connected()
         return True if connected else False
-
-    @commands.command(name="disconnect", pass_ctx=True)
-    async def disconnect(self, ctx):
-        await ctx.voice_client.disconnect()
-
-    @commands.command(name='t1', pass_ctx=True)
-    async def t1(self, ctx: discord.ext.commands.context.Context):
-        await self.play(ctx, t1)
-
-    @commands.command(name='t2', pass_ctx=True)
-    async def t2(self, ctx: discord.ext.commands.context.Context):
-        await self.play(ctx, t2)
-
-    @commands.command(name="op", pass_ctx=True)
-    async def op(self, ctx, num):
-        try:
-            int(num)
-            if int(num) > 24 or int(num) < 1:
-                raise ValueError
-            playing = await self.play(ctx, OPS[num])
-            if playing:
-                await ctx.send("Playing %s." % ("op %s" % num))
-        except ValueError:
-            if num != "all":
-                await ctx.send("Bad argument %s!" % num)
-            else:
-                # todo: play all openings shuffled
-                pass
-
-    def get_voice(self, ctx: discord.ext.commands.context.Context):
-        return discord.utils.get(self.client.voice_clients, guild=ctx.guild)
-
-    @commands.command(name="stop", pass_ctx=True)
-    async def stop_playing(self, ctx: discord.ext.commands.context.Context):
-        voice = self.get_voice(ctx)
-        voice.stop()
-
-    def is_playing(self, ctx: discord.ext.commands.context.Context):
-        voice = self.get_voice(ctx)
-        return voice.is_playing()
-
-    async def handle_connected(self, ctx: discord.ext.commands.context.Context):
-        if not self.is_connected(ctx):
-            await ctx.send("Joining channel.")
-            await self.join(ctx)
-
-    async def send_playing(self, ctx: discord.ext.commands.context.Context):
-        await ctx.send("Added to queue.") if self.is_playing(ctx) else await ctx.send("The song will be played now")
 
     @staticmethod
     async def get_audio(url):
@@ -101,10 +40,8 @@ class Music(commands.Cog):
         return source
 
     async def idle_speaker(self, ctx: discord.ext.commands.context.Context):
-        self.thread = True
         while self.is_playing(ctx):
             time.sleep(1)
-        self.thread = False
         await self.play_queue(ctx)
 
     def wait_for_idle(self, ctx: discord.ext.commands.context.Context):
@@ -163,6 +100,68 @@ class Music(commands.Cog):
                 await ctx.send("Nothing is paused.")
         except TypeError:
             pass
+
+    @commands.command(name="disconnect", pass_ctx=True)
+    async def disconnect(self, ctx):
+        await ctx.voice_client.disconnect()
+
+    @commands.command(name='t1', pass_ctx=True)
+    async def t1(self, ctx: discord.ext.commands.context.Context):
+        await self.play(ctx, t1)
+
+    @commands.command(name='t2', pass_ctx=True)
+    async def t2(self, ctx: discord.ext.commands.context.Context):
+        await self.play(ctx, t2)
+
+    @commands.command(name="op", pass_ctx=True)
+    async def op(self, ctx, num):
+        try:
+            int(num)
+            if int(num) > 24 or int(num) < 1:
+                raise ValueError
+            playing = await self.play(ctx, OPS[num])
+            if playing:
+                await ctx.send("Playing %s." % ("op %s" % num))
+        except ValueError:
+            if num != "all":
+                await ctx.send("Bad argument %s!" % num)
+            else:
+                # todo: play all openings shuffled
+                pass
+
+    def get_voice(self, ctx: discord.ext.commands.context.Context):
+        return discord.utils.get(self.client.voice_clients, guild=ctx.guild)
+
+    @commands.command(name="join", pass_ctx=True)
+    async def join(self, ctx):
+        voice_channel = None
+        if ctx.author.voice is None:
+            await ctx.send("You're not in a voice channel.")
+            return
+        else:
+            voice_channel = ctx.author.voice.channel
+        if ctx.voice_client is None:
+            await voice_channel.connect()
+        else:
+            await ctx.voice_client.move_to(voice_channel)
+
+    @commands.command(name="stop", pass_ctx=True)
+    async def stop_playing(self, ctx: discord.ext.commands.context.Context):
+        voice = self.get_voice(ctx)
+        voice.stop()
+        self.queue = []
+
+    def is_playing(self, ctx: discord.ext.commands.context.Context):
+        voice = self.get_voice(ctx)
+        return voice.is_playing()
+
+    async def handle_connected(self, ctx: discord.ext.commands.context.Context):
+        if not self.is_connected(ctx):
+            await ctx.send("Joining channel.")
+            await self.join(ctx)
+
+    async def send_playing(self, ctx: discord.ext.commands.context.Context):
+        await ctx.send("Added to queue.") if self.is_playing(ctx) else await ctx.send("The song will be played now")
 
 
 def setup(client):
